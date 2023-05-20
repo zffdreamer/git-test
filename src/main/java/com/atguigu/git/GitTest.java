d19236cf2911216c25a2bce0505fe6cf09b28c55
ZfDreamer <1947028074@qq.com>
2023-05-20 23:38:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 郑锋
+ * @date 2023/5/20 23:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
