67d06cb6c96ca414edaa76b8503b2d54a4c49f37
ZfDreamer <1947028074@qq.com>
2023-05-21 10:53:36 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println("master commit");
+        System.out.println("push commit");
     }
 }
